818c05ee9d95a1472f27a55e5f3264ca89ebda13
Darren Neimke <darren.neimke@gmail.com>
2020-12-01 13:39:10 +1030

    Replaced Markdown with Markdig

--- MarkdownEditor/Program.cs
+++ MarkdownEditor/Program.cs
@@ -1,4 +1,3 @@
-using HeyRed.MarkdownSharp;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -14,7 +13,6 @@ namespace MarkdownEditor
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped<Markdown>();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
